b8a4ac024f380abb8257d2cd43fa944786334fe1
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2021-11-29 23:50:15 -0600

    #1 stub project

---
+++ servicebusreplayer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace servicebusreplayer
+{
+    class Program
+    {
+        //TODO: DI queue service
+        static async Task<int> Main(string[] args)
+        {
+            //TODO: parse input and do stuff...
+            var parsedAlias = "post-dumb-message";
+
+            var dlqMessages = GetDLQMessagesFromQueue(parsedAlias);
+            
+            return 0;
+        }
+
+        static List<DlqMessage> GetDLQMessagesFromQueue(string queueAlias)
+        {
+            
+            var mockDLQMessages = new List<DlqMessage>
+            {
+                new DlqMessage
+                {
+                    MessageId = Guid.NewGuid(),
+                    MessageBody = "Some message"
+                }
+            };
+
+            foreach (var dlqMsg in mockDLQMessages)
+            {
+                Console.WriteLine($"MessageId: {dlqMsg.MessageId} MessageBody:{dlqMsg.MessageBody}");
+            }
+            return mockDLQMessages;
+        }
+    }
+    class DlqMessage
+    {
+        public Guid MessageId { get; set; }
+        public string MessageBody { get; set; }
+    }
+
+}
